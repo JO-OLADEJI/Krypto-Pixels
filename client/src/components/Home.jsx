b8317b69278b6ccd2115f8882e7a851ce7dcb573
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-01-16 22:27:45 +0100

    added images and short writeups

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,15 +3,21 @@ import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import styles from './Home.module.css';
 import Roadmap from './Roadmap.jsx';
 
+// images
+import hunter from '../assets/hunter.png';
+import mage from '../assets/mage.png';
+import ninja from '../assets/ninja.png';
+import warrior from '../assets/warrior.png';
+
 const Home = (props) => {
   return (
     <main className={styles['home']}>
       <ParallaxProvider>
         <Parallax 
-          y={[-50, 50]} 
+          y={[-30, 50]} 
           className={styles['hero']}>
           <div className={styles['background']}>
-            <h1>Hero</h1>
+            <h2>Hero</h2>
           </div>
         </Parallax>
 
@@ -19,7 +25,18 @@ const Home = (props) => {
           y={[10, -10]} 
           className={`${styles['typ']} ${styles['hunter']}`}>
           <div className={styles['type']}>
-            <h2>Hunter</h2>
+            <div className={styles['nft-art']}>
+              <img 
+                src={hunter}
+                alt="Hunter's image" 
+              />
+            </div>
+            <div className={styles['nft-description']}>
+              <h2>Hunter</h2>
+              <p>
+                The scouts of our domain, fast, at ease in nature, comfortable moving alone, good with beasts. They go ahead of our armies and find a way.
+              </p>
+            </div>
           </div>
         </Parallax>
 
@@ -27,7 +44,18 @@ const Home = (props) => {
           y={[5, -5]} 
           className={`${styles['typ']} ${styles['warrior']}`}>
           <div className={styles['type']}>
-            <h2>Warrior</h2>
+            <div className={styles['nft-art']}>
+              <img 
+                src={warrior}
+                alt="Warrior's image" 
+              />
+            </div>
+            <div className={styles['nft-description']}>
+              <h2>Warrior</h2>
+              <p>
+                Drawn from space and time, strong, slight disorderly, at ease in chaos. They smash through opposition with zero fear or concern for personal safety.
+              </p>
+            </div>
           </div>
         </Parallax>
 
@@ -35,7 +63,18 @@ const Home = (props) => {
           y={[50, -50]} 
           className={`${styles['typ']} ${styles['mage']}`}>
           <div className={styles['type']}>
-            <h2>Mage</h2>
+            <div className={styles['nft-art']}>
+              <img 
+                src={mage}
+                alt="Mage's image" 
+              />
+            </div>
+            <div className={styles['nft-description']}>
+              <h2>Mage</h2>
+              <p>
+                Powerful and wise, they control the elements and know the future. Yet not they, but the Mysterious Higher Power they follow.
+              </p>
+            </div>
           </div>
         </Parallax>
 
@@ -43,7 +82,18 @@ const Home = (props) => {
           y={[-10, 10]} 
           className={`${styles['typ']} ${styles['ninja']}`}>
           <div className={styles['type']}>
-            <h2>Ninja</h2>
+            <div className={styles['nft-art']}>
+              <img 
+                src={ninja}
+                alt="Ninja's image" 
+              />
+            </div>
+            <div className={styles['nft-description']}>
+              <h2>Ninja</h2>
+              <p>
+                Oh yes, we have ninjas.. invisible, quiet... neither seen or hears, assassins, spies, spies in disguise...
+              </p>
+            </div>
           </div>
         </Parallax>
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './Nav.module.css';
 import { Link } from 'react-router-dom';
-import openseaLogo from '../assets/opensea-logo.png';
 
 const Nav = (props) => {
   return (
